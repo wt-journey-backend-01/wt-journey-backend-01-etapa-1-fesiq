c62521d73b351331d81b43a30a4be888f0a0a2eb
Fernando Siqueira <fernandosiqueira994@gmail.com>
2025-07-16 00:03:45 -0300

    adicionando static e urlencoded e as rotas com sendFile

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public')); //pra servir arquivos estáticos da pasta public
+app.use(express.urlencoded({ extended: true })); //pra processar dados de formulários
+
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/contato', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contato.html'));
+});
+
+app.get('/api/lanches', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'data', 'lanches.json'));
 });
 
 app.listen(PORT, () => {
