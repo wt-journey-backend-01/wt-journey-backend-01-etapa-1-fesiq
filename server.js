c92228f5b4b99a537954b95a1ca3c858d8de58df
Fernando Siqueira <fernandosiqueira994@gmail.com>
2025-07-16 00:06:20 -0300

    esqueci de definir path ':D

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const path = require('path');
 const PORT = 3000;
 
 app.use(express.static('public')); //pra servir arquivos estáticos da pasta public
